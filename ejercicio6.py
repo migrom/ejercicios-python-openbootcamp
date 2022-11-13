4fb537166787256b9cf0a925cfcf157769fb63a7
migrom <miguelrop@gmail.com>
2022-11-13 23:50:58 +0100

    Add files via upload

---
+++ ejercicio6.py
@@ -0,0 +1,33 @@
+"""
+En este ejercicio vais a crear la clase Vehículo la cual tendrá los siguientes atributos:
+    Color
+    Ruedas
+    Puertas
+
+Por otro lado crearéis la clase Coche la cual heredará de Vehículo y tendrá los siguientes atributos:
+    Velocidad
+    Cilindrada
+
+Por último, tendrás que crear un objeto de la clase Coche y mostrarlo por consola.
+"""
+
+class Vehiculo():
+    def Color(self):
+        pass
+    def Ruedas(self):
+        pass
+    def Puertas(self):
+        pass
+
+class Coche(Vehiculo):
+    def Velocidad(self):
+        pass
+    def Cilindrada(self):
+        pass
+
+micoche = Coche()
+
+print (dir(micoche))
+
+
+
